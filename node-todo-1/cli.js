c88df48a4051952354805ad941dadfc21cc5a15f
Li-Gen <1030052539@qq.com>
2021-03-15 00:24:46 +0800

    clear

--- node-todo-1/cli.js
+++ node-todo-1/cli.js
@@ -1,4 +1,4 @@
-const { program } = require("commander");
+const {program} = require("commander");
 const api = require('./index.js');
 
 // 添加option
@@ -6,18 +6,34 @@ program.option("-x, --xxx", "this is new option");
 
 // 添加add子命令
 program
-  .command("add <tasks...>")
-  .description("add a task")
-  .action(function (tasks) {
-    api.add(tasks).then()
-  });
+    .command("add <tasks...>")
+    .description("add a task")
+    .action(function (tasks) {
+        api.add(tasks).then(() => {
+            console.log("添加成功")
+        }, () => {
+            console.log("添加失败")
+        })
+    });
 
 // 添加clear子命令
 program
-  .command("clear")
-  .description("clear all tasks")
-  .action(() => {
-    console.log("clear all tasks")
-  });
+    .command("clear")
+    .description("clear all tasks")
+    .action(() => {
+        api.clear().then(() => {
+            console.log("清除成功")
+        }, () => {
+            console.log("清除失败")
+        })
+    });
+
+if (process.argv.length === 2) {
+    console.log("我进来了")
+    void api.showAll()
+    console.log("66666")
+}
 
 program.parse(process.argv);
+
+
--- node-todo-1/index.js
+++ node-todo-1/index.js
@@ -1,6 +1,6 @@
 const db = require('./db.js');
-
-module.exports.add = async function (tasks) {
+// 添加todo
+module.exports.add = async(tasks)=> {
     // 1. 读取之前的任务
     const list = await db.read();
     // 2. 往里面添加tasks任务
@@ -10,3 +10,22 @@ module.exports.add = async function (tasks) {
     // 3. 存储任务到文件
     await db.write(list);
 };
+// 清除全部todo
+module.exports.clear = async()=>{
+    await db.write([])
+}
+// 展示全部todo
+module.exports.showAll = async()=>{
+    console.log(db.read().then((result)=>{
+        console.log(result);},(err)=>{
+        console.log(err);}));
+    console.log("888");
+    // 1. 读取之前的todo
+    const list = await db.read();
+    console.log(list)
+    console.log("7777");
+    // 2. 打印之前的todo
+    list.forEach((task,index)=>{
+        console.log(`${index + 1}-${task}`)
+    })
+}
