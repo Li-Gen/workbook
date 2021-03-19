a258841a2ea3848659d70dd1760c6aac768f6a66
Reagen <1030052539@qq.com>
2021-03-19 16:30:15 +0800

    all

--- node-todo-reagen/__mocks__/fs.js
+++ node-todo-reagen/__mocks__/fs.js
@@ -1,9 +1,11 @@
+// 假的fs其实和真的fs一模一样只是假的fs操作的不是真的文件而是在jest提供给我们的空间里玩耍
 const fs = jest.createMockFromModule('fs'); //  假的fs模块
 const _fs = jest.requireActual('fs');       // 真正的fs模块
 
 Object.assign(fs, _fs);                                  // 假的fs拷贝真的fs模块中的所有API
 
 const mocks = {};
+// 返回的是error和data的数组
 fs.setMock = (path, error, data) => {
     mocks[path] = [error, data];
 };
@@ -13,9 +15,11 @@ fs.readFile = (path, option, callback) => {
     if (callback === undefined) {
         callback = option
     }
-    // 说明调用了fs.setMock
+    // 说明用户想要进行mock
     if (path in mocks) {
+    // 返回函数返回的参数
         callback(mocks[path][0], mocks[path][1])
+    // 否则就调用真正的读文件
     } else {
         _fs.readFile(path, option, callback)
     }
--- node-todo-reagen/__test__/db.spec.js
+++ node-todo-reagen/__test__/db.spec.js
@@ -2,13 +2,15 @@
 const db = require('../db');
 // 一定要注意这里require的fs不是真正的fs，而是jest接管的假的fs
 const fs = require('fs');
-// jest造的一个假的fs专门用来做测试用的
+// jest造的一个假的fs专门用来做测试用的,之后的fs都是自己造的假的fs
 jest.mock('fs');
 
 describe('db',() => {
     it('can read', async function () {
         const data = [{"title": 'Hi node.js',"done":false}];
+        // 只要读取这个路径就返回error为空，读取的内容为data
         fs.setMock("/xxx",null,JSON.stringify(data));
+        // read方法调用的是假的fs.readFile()
         const list = await db.read('/xxx');
         expect(list).toStrictEqual(data)
     });
