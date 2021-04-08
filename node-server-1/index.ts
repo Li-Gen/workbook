7cc19f17ecaaf1a09d63b6ac4f0e6c36c0b1e3a5
Reagen <1030052539@qq.com>
2021-04-08 11:01:53 +0800

    处理不存在的文件并返回404页面

--- node-server-1/index.ts
+++ node-server-1/index.ts
@@ -13,13 +13,30 @@ server.on("request", (request: IncomingMessage, response: ServerResponse) => {
     // 使用WHATWG的URL拿到url和查询参数
     const {pathname, search} = new URL(request.url, `http://${request.headers.host}`);
     // 拿到用户输入的任意路径
-    const fileName = pathname.substr(1);
+    let fileName = pathname.substr(1);
+    // 如果输入的路径是空的默认打开index.html
+    if(fileName === ''){
+        fileName = 'index.html'
+    }
     fs.readFile(p.resolve(publicDir, fileName), (error, data) => {
+        response.setHeader("Content-Type", "text/html;charset=utf-8");
         if (error) {
-            response.statusCode = 404;
-            response.end('你访问的网页搬家了！')
+            console.log("error", error);
+            if (error.errno === -4058) {
+                // 如果404了就读一下404页面并将结果返回
+                fs.readFile(p.resolve(publicDir, '404.html'),(error,data)=>{
+                    response.statusCode = 404;
+                    response.end(data);
+                })
+            } else if(error.errno === -4068){
+                response.statusCode = 403;
+                response.end('不允许访问一个目录');
+            }else {
+                response.statusCode = 500;
+                response.end('服务器不存在，请稍后再试');
+            }
         } else {
-            response.end(data.toString());
+            response.end(data);
         }
     });
 });
