059ed30938e034b3c103bf12b2c7bf51bf863879
Reagen <5235360+reagen@user.noreply.gitee.com>
2021-05-08 10:49:33 +0800

    stream

---
+++ stream1/1.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const stream = fs.createWriteStream('./big_file.txt');
+for (let i = 0; i < 10000000; i++) {
+    stream.write(`这是第${i + 1}行数据，我们还需要很多数据回车\n`)
+}
+stream.end();
+console.log('done');
---
+++ stream1/3.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const http = require('http');
+const server = http.createServer();
+server.on('request', (request, response) => {
+    const stream = fs.createReadStream('./big_file.txt');
+    stream.pipe(response)
+});
+server.listen(8888);
+
+console.log(8888);
